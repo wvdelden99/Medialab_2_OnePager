1f6b40031521c4a291d52c2e45a066ae317bc313
WvDeldenHR <wvdelden99@outlook.com>
2024-05-13 16:05:51 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p className="bg-black">test</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
